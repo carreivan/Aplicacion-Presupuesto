7074b19faa20069f50945ab871f46d53aa073aac
Ivan Carreño <carreaguileraivan@gmail.com>
2022-09-22 18:12:56 -0300

    Adicionando Listado de Ingresos con Javascript

--- js/app.js
+++ js/app.js
@@ -1,46 +1,81 @@
 const ingresos = [
-    new Ingreso('Salario', 2100),
-    new Ingreso('Venta auto', 1500)
+  new Ingreso("Salario", 2100),
+  new Ingreso("Venta auto", 1500),
 ];
 
-const egresos =[
-    new Egreso('Renta departamento', 900),
-    new Egreso('Ropa', 400)
+const egresos = [
+  new Egreso("Renta departamento", 900),
+  new Egreso("Ropa", 400),
 ];
 
-cargarApp = ()=>{
-    cargarCabecero();
-}
+cargarApp = () => {
+  cargarCabecero();
+  cargarIngresos();
+};
 
-let totalIngresos = ()=>{
-    let totalIngreso = 0;
+let totalIngresos = () => {
+  let totalIngreso = 0;
+  for (let ingreso of ingresos) {
+    totalIngreso += ingreso.valor;
+  }
+  return totalIngreso;
+};
+
+let totalEgresos = () => {
+  let totalEgreso = 0;
+  for (let egreso of egresos) {
+    totalEgreso += egreso.valor;
+  }
+  return totalEgreso;
+};
+
+let cargarCabecero = () => {
+  let presupuesto = totalIngresos() - totalEgresos();
+  let procentajeEgreso = totalEgresos() / totalIngresos();
+  document.getElementById("presupuesto").innerHTML = formatoMoneda(presupuesto);
+  document.getElementById("porcentaje").innerHTML =
+    formatoPorcentaje(procentajeEgreso);
+  document.getElementById("ingresos").innerHTML = formatoMoneda(
+    totalIngresos()
+  );
+  document.getElementById("egresos").innerHTML = formatoMoneda(totalEgresos());
+};
+
+const formatoMoneda = (valor) => {
+  return valor.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  });
+};
+
+const formatoPorcentaje = (valor) => {
+  return valor.toLocaleString("en-US", {
+    style: "percent",
+    minimumFractionDigits: 2,
+  });
+};
+
+const cargarIngresos= ()=>{
+    let ingresosHTML = '';
     for(let ingreso of ingresos){
-        totalIngreso += ingreso.valor;
+        ingresosHTML += crearIngresoHTML(ingreso);
     }
-    return totalIngreso;
+    document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
 }
 
-let totalEgresos = ()=>{
-    let totalEgreso = 0;
-    for(let egreso of egresos){
-        totalEgreso += egreso.valor;
-    }
-    return totalEgreso;
-}
-
-let cargarCabecero = ()=>{
-let presupuesto = totalIngresos() - totalEgresos();
-let procentajeEgreso = totalEgresos() / totalIngresos();
-document.getElementById('presupuesto').innerHTML =formatoMoneda(presupuesto);
-document.getElementById('porcentaje').innerHTML = formatoPorcentaje(procentajeEgreso);
-document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
-document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
-}
-
-const formatoMoneda = (valor) =>{
-    return valor.toLocaleString('en-US', {style:'currency', currency:'USD', minimumFractionDigits:2});
-}
-
-const formatoPorcentaje = (valor)=>{
-    return valor.toLocaleString('en-US',{style:'percent',minimumFractionDigits:2});
+const crearIngresoHTML = (ingreso)=>{
+    let ingresosHTML = `
+    <div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${ingreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+    <div class="elemento_valor">${formatoMoneda(ingreso.valor)}</div>
+    <div class="elemento_eliminar">
+    <button class="elemento_eliminar--btn">
+    <ion-icon name="close-circle-outline"></ion-icon>
+    </button>
+    </div>
+    </div>
+    </div> `;
+    return ingresosHTML;
 }
