dfb0ff22ef9204e6b8abb8eba95229ce4b958422
Ivan Carreño <carreaguileraivan@gmail.com>
2022-09-22 16:58:47 -0300

    Adicionando codigo de  App.js primera parte fucniones principales

--- js/Ingresos.js
+++ js/Ingresos.js
@@ -1,9 +1,9 @@
 class Ingreso  extends Dato{
-    static contadoIngresos = 0;
+    static contadorIngresos = 0;
     
     constructor(descripcion, valor){
         super(descripcion, valor);
-        this._id = ++Ingreso.contadoIngresos;
+        this._id = ++Ingreso.contadorIngresos;
     }
     get id(){
         return this._id;
